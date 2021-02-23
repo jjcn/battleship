775128cc864c4886cd8b6683b277f4d1dc5b7d2b
Shengxi Jin <sj320@duke.edu>
2021-02-23 01:01:52 -0500

    task1

---
+++ src/main/java/edu/duke/sj320/battleship/Board.java
@@ -0,0 +1,34 @@
+package edu.duke.sj320.battleship;
+
+/**
+ * The interface of a board to play "Battleship".
+ *
+ * @param <T> is the generic describing the implementation of board
+ */
+public interface Board<T> {
+  public int getWidth();
+
+  public int getHeight();
+  
+  public String tryAddShip(Ship<T> toAdd);
+  
+  public Ship<T> getShipAt(Coordinate where);
+  /**
+   * Display the status of a coordinate as it is seen by myself
+   * @param where is the coordinate to check
+   * @return display info it has at the coordinate
+   */
+  public T whatIsAtForSelf(Coordinate where);
+  
+  /**
+   * Display the status of a coordinate as it is seen by enemy
+   * @param where is the coordinate to check
+   * @return display info it has at the coordinate
+   */
+  public T whatIsAtForEnemy(Coordinate where);
+  
+  public Ship<T> fireAt(Coordinate c);
+
+  public boolean checkLose();
+}
+
